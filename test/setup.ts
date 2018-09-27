4ac80f78513147735fae7620b0fbaa46a26ce8be
Justin Fan <jjustinfan1997@gmail.com>
2018-09-27 17:50:45 -0500

    Initial commit.

---
+++ test/setup.ts
@@ -0,0 +1,11 @@
+// we always make sure 'react-native' gets included first
+import "react-native"
+
+// libraries to mock
+import "./mock-i18n"
+import "./mock-reactotron"
+import "./mock-textinput"
+
+declare global {
+  var __TEST__
+}
