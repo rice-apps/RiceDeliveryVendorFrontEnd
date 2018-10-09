f88dd9c86051ed5ed887d5198a2a263295a0eb11
Jonathan Cai <caimjonathan@gmail.com>
2018-10-09 10:51:25 -0500

    navigation changes

--- src/navigation/example-navigator.ts
+++ src/navigation/example-navigator.ts
@@ -1,10 +1,12 @@
 import { createStackNavigator } from "react-navigation"
 import { FirstExampleScreen } from "../views/example/first-example-screen"
 import { SecondExampleScreen } from "../views/example/second-example-screen"
+import { ThirdExampleScreen } from "../views/example/third-example-screen"
 
 export const ExampleNavigator = createStackNavigator({
   firstExample: { screen: FirstExampleScreen },
   secondExample: { screen: SecondExampleScreen },
+  thirdExample: { screen: ThirdExampleScreen },
 },
 {
   headerMode: "none",
