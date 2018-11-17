a6031658634ff670368faf3d530c6a26e84f52a2
Justin Fan <jjustinfan1997@gmail.com>
2018-11-17 15:05:21 -0600

    got order query to work.

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -5,11 +5,23 @@
 import { AppRegistry } from "react-native"
 import { RootComponent } from "./root-component"
 import { StorybookUIRoot } from "../../storybook"
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 /**
  * This needs to match what's found in your app_delegate.m and MainActivity.java.
  */
 const APP_NAME = "testIgniteProject"
+
+const link = createHttpLink({
+  uri: 'http://localhost:3000/graphql'
+})
+export const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache()
+});
 
 // Should we show storybook instead of our app?
 //
