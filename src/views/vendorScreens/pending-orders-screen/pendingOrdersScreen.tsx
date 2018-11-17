882a4ae0ad50a3b434b3f010e590ac0f4369215e
Amy Huyen <awh4@rice.edu>
2018-11-17 15:05:44 -0600

    working on orders page

--- src/views/vendorScreens/pending-orders-screen/pendingOrdersScreen.tsx
+++ src/views/vendorScreens/pending-orders-screen/pendingOrdersScreen.tsx
@@ -1,16 +1,48 @@
 
 import * as React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Button} from 'react-native';
 
 export class PendingOrdersScreen extends React.Component<any, any> {
   render() {
     return (
       <View>
-        <Text>
-          Pending orders Screen
-        </Text>
+        <FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem={({item}) => 
+        <View>
+          <Text style={styles.item}>{item.key}</Text>
+          <Button
+            onPress= {this.changeOrderStatus}
+            title="Change Order Status"
+           />
+        </View>}
+        />
       </View>
     
       )
   }
+
+  changeOrderStatus() {
+
+  }
 }
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
