f9e9cda08ded98c5c86f9080a324f417fd5a26f8
William Su <ts47@rice.edu>
2019-02-02 13:01:29 -0600

    changed code back to the state before previous commit, read to merge to master

--- src/app/stores/vendor-store.ts
+++
@@ -1,7 +0,0 @@
-import { types } from "mobx-state-tree"
-
-export const Vendor = types.model("Vendor", {
-    name: types.string,
-    hours: types.array(types.array(types.number)),
-    // batchOrder: types.array(types.string)
-  })
